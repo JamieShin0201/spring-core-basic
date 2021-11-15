4458be2cf65fd1a841e14eb1503ecab1fd579185
JamieShin0201 <jamieshin1450@gmail.com>
2021-11-15 23:24:20 +0900

    feat: 관심사 분리
    - AppConfig에서 전체적인 구성을 책임 지도록 관심사를 분리함
    - 각 서비스는 인터페이스만 의존
    - 애플리케이션이 크게 사용영역과, 객체를 생성하고 구성하는 영역으로 분리됨

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,18 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    // 인터페이스뿐만 아니라 구체 클래스에도 의존한다. (DIP 위반)
-    // 구체 클래스를 변경하려면 OrderServiceImpl(클라이언트)를 수정해야한다. (OCP 위반)
-    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
